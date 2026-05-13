3ecd90305350b16182ede741cd988b32f7c8f090
vinimeurer <vinnymeurer@gmail.com>
2026-05-13 17:03:44 -0300

    Refactor preprocessing pipeline to make markdown processing, section handling text cleaning  and add table processing functionalities.

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,5 +1,13 @@
 
 from .preprocessor import Preprocessor, ProcessResult
-from .text_preprocessor import TextPreprocessor
+from .section_parser import SectionParser
+from .text_cleaner import TextCleaner
+from .table_processor import TableProcessor
 
-__all__ = ["Preprocessor", "ProcessResult", "TextPreprocessor"]
+__all__ = [
+    "Preprocessor",
+    "ProcessResult",
+    "SectionParser",
+    "TextCleaner",
+    "TableProcessor",
+]
